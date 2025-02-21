9f1884874e72bd3fe91b08e178664ad08203883e
karthick sankar <karthick.s@lantrasoft.com>
2025-02-21 11:52:10 +0530

    added tha app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,117 @@
-import Image from "next/image";
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { useState } from 'react';
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+  const [task, setTask] = useState<
+    { taskID: number; taskName: string; status: boolean }[]
+  >([]);
+  function handleDeleteTask(taskId: number) {
+    setTask(task.filter((item) => item.taskID !== taskId));
+  }
+  function handleTaskUpdate(taskId: number) {
+    setTask(
+      task.map((item) =>
+        item.taskID === taskId ? { ...item, status: !item.status } : item
+      )
+    );
+  }
+  function handleAddTask() {
+    setTask([
+      ...task,
+      {
+        taskID: task.length + 1,
+        taskName: inputValue,
+        status: false,
+      },
+    ]);
+    setInputValue('');
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col items-center min-h-screen">
+      <div className="w-full max-w-md mt-32 px-4">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-primary mb-2">ToDo List</h1>
+          <p className="text-muted-foreground">
+            Keep track of your daily tasks
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="flex gap-3">
+          <Input
+            type="text"
+            value={inputValue}
+            placeholder="Add Task"
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          ></Input>
+          <Button onClick={handleAddTask}>Add Task</Button>
+        </div>
+        <div className="mt-8">
+          <TaskTable
+            task={task}
+            handleUpdateTask={handleTaskUpdate}
+            handleDeleteTask={handleDeleteTask}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
+  );
+}
+
+interface TaskTableProps {
+  task: { taskID: number; taskName: string; status: boolean }[];
+  handleUpdateTask: (taskId: number) => void;
+  handleDeleteTask: (taskId: number) => void;
+}
+
+function TaskTable({
+  task,
+  handleUpdateTask,
+  handleDeleteTask,
+}: TaskTableProps) {
+  return task.length > 0 ? (
+    <Table>
+      <TableCaption>Finish Task! Be Productive</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Update</TableHead>
+          <TableHead className="w-[400px]">Task</TableHead>
+          <TableHead>Remove</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {task.map((item) => (
+          <TableRow key={item.taskID}>
+            <TableCell className="font-medium">
+              <Checkbox onCheckedChange={() => handleUpdateTask(item.taskID)} />
+            </TableCell>
+            <TableCell className={item.status ? 'line-through' : ''}>
+              {item.taskName}
+            </TableCell>
+
+            <TableCell>
+              <Button onClick={() => handleDeleteTask(item.taskID)}>
+                Delete
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  ) : (
+    <p className="font-light">Add your task to start!</p>
   );
 }
